52716822b9d2b4c89ccc74e2b1731fc5ebcc2dac
Lucas Pinheiro <pinheiro.lucasaugusto@gmail.com>
2016-07-05 21:20:11 -0300

    Add labels to ScorePanel

--- TrucoJava/src/ui/ScorePanel.java
+++ TrucoJava/src/ui/ScorePanel.java
@@ -10,7 +10,10 @@ import java.awt.*;
  * @author Bruno Vedovetto @bleandro
  */
 public class ScorePanel extends JPanel {
-    // TODO: investigate which is the best component to hold the score for the game
+    private JLabel player1GameScore = new JLabel();
+    private JLabel player2GameScore = new JLabel();
+    private JLabel player1RoundScore = new JLabel();
+    private JLabel player2RoundScore = new JLabel();
 
     /**
      * Constructor of the class
@@ -18,10 +21,67 @@ public class ScorePanel extends JPanel {
     ScorePanel() {
         super();
         this.setInitialOpts();
+        this.setLabelsOptions();
+        this.setInitialScores();
+    }
+
+    private void setInitialScores() {
+        this.setPlayer1GameScore(0);
+        this.setPlayer2GameScore(0);
+        this.setPlayer1RoundScore(0);
+        this.setPlayer2RoundScore(0);
+    }
+
+    public void setPlayer1RoundScore(int score) {
+        this.player1RoundScore.setText("Player 1 ==> " + score);
+    }
+
+    public void setPlayer2RoundScore(int score) {
+        this.player2RoundScore.setText("Player 1 ==> " + score);
+    }
+
+    private void setLabelsOptions() {
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+
+        this.add(new JLabel("GAME SCORE"), constraints);
+
+        constraints.gridy = 1;
+        this.add(this.player1GameScore, constraints);
+
+        constraints.gridy = 2;
+        this.add(this.player2GameScore, constraints);
+
+        constraints.gridy = 3;
+        this.add(new JLabel("ROUND SCORE"), constraints);
+
+        constraints.gridy = 4;
+        this.add(this.player1RoundScore, constraints);
+
+        constraints.gridy = 5;
+        this.add(this.player2RoundScore, constraints);
+    }
+
+    /**
+     * Display the new player 1 score
+     * @param score {int}
+     */
+    public void setPlayer1GameScore(int score) {
+        this.player1GameScore.setText("Player 1 ==> " + score);
+    }
+
+    /**
+     * Set the new player 1 score
+     * @param score {int}
+     */
+    public void setPlayer2GameScore(int score) {
+        this.player2GameScore.setText("Player 2 ==> " + score);
     }
 
     private void setInitialOpts() {
+        this.setLayout(new GridBagLayout());
         this.setPreferredSize(new Dimension(GamePanel.DEFAULT_PANEL_WIDTH, GamePanel.DEFAULT_PANEL_HEIGHT));
-        this.setBackground(Color.RED);
+        this.setBackground(Color.WHITE);
     }
 }
